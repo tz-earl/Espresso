75d8a4297b9d1cf68f66df2a6d85360e446928c1
Earl Fong <earl_fong@operamail.com>
2020-09-14 20:02:25 -0700

    Implement /restaurants/<rest_id> endpoint

--- espresso.py
+++ espresso.py
@@ -36,33 +36,67 @@ class Restaurant(db.Model):
 # Controllers.
 #----------------------------------------------------------------------------#
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return 'Hello from espresso'
 
-@app.route('/restaurants')
+def restaurant_to_json(rest):
+    """Convert a Restaurant object to json object"""
+    rest_item = {'id': rest.id, 'name': rest.name, 'street': rest.street, 'suite': rest.suite,
+                 'city': rest.city, 'state': rest.state, 'zip_code': rest.zip_code,
+                 'phone_num': rest.phone_num, 'website': rest.website, 'email': rest.email,
+                 'date_established': rest.date_established
+                 }
+    return rest_item
+
+@app.route('/restaurants', methods=['GET'])
 def restaurants():
     try:
         restaurants = Restaurant.query.filter_by().order_by(Restaurant.id)
+        rest_list = []
+        for rest in restaurants:
+            rest_item = restaurant_to_json(rest)
+            rest_list.append(rest_item)
     except Exception as ex:
         print('Failed to retrieve list of restaurants for "/restaurants" endpoint')
         print(f'Exception was thrown: {str(ex)}')
         ret_val = {'success': False,
-            'message': 'List of restaurants could not be retrieved',
+            'message': 'Server failure: list of restaurants could not be retrieved',
             'error-string': str(ex)
             }
+        return jsonify(ret_val), 500
     else:
-        rest_list = []
-        for rest in restaurants:
-            rest_item = {'id':rest.id, 'name':rest.name, 'street':rest.street, 'suite':rest.suite,
-                         'city':rest.city, 'state':rest.state, 'zip_code':rest.zip_code,
-                         'phone_num':rest.phone_num, 'website':rest.website, 'email':rest.email,
-                         'date_established':rest.date_established
-                         }
-            rest_list.append(rest_item)
         ret_val = {'success': True, 'restaurants': rest_list}
-    finally:
-        return jsonify(ret_val)
+        return jsonify(ret_val), 200
+
+@app.route('/restaurants/<rest_id>', methods=['GET'])
+def restaurant_by_id(rest_id):
+    try:
+        rest = Restaurant.query.get(rest_id)
+    except Exception as ex:
+        print(f'Failed to retrieve restaurant for "/restaurants/{id}" endpoint')
+        print(f'Exception was thrown: {str(ex)}')
+        ret_val = {'success': False,
+            'message': f'Server failure: restaurant with id number {id} could not be retrieved',
+            'error-string': str(ex)
+            }
+        return jsonify(ret_val), 500
+    else:
+        if rest:
+            rest_item = restaurant_to_json(rest)
+            ret_val = {'success': True, 'restaurant': rest_item}
+            return jsonify(ret_val), 200
+        else:
+            ret_val = {'success': False, 'message': f'No restaurant with id {rest_id} found'}
+            return jsonify(ret_val), 404
+
+@app.errorhandler(404)
+def not_found(error):
+    ret_val = {'success': False,
+               'message': 'Resource was not found, check the url',
+               'error-string': str(error)
+               }
+    return jsonify(ret_val), 404
 
 #----------------------------------------------------------------------------#
 # Launch.
