f0abd62009bc39f7c79c7dc625847538e3c80172
Earl Fong <earl_fong@operamail.com>
2020-10-08 21:49:10 -0700

    Implement update restaurant

--- espresso.py
+++ espresso.py
@@ -140,6 +140,60 @@ def restaurant_create():
         ret_val = {'success': True, 'message': f'Restaurant created with name: {rest.name}'}
         return jsonify(ret_val), 200
 
+@app.route('/restaurants/<rest_id>', methods=['PUT'])
+def restaurant_update(rest_id):
+    try:
+        rest = Restaurant.query.get(rest_id)
+    except sqlalchemy_exceptions.ProgrammingError as ex:
+        logging.error(f'Failed to retrieve restaurant for update for "/restaurants/{rest_id}" endpoint')
+        logging.error(f'Exception was thrown: {str(ex)}')
+        ret_val = {'success': False,
+                   'message': f'Server failure: restaurant with id number {rest_id} could not be retrieved',
+                   }
+        return jsonify(ret_val), 500
+    except sqlalchemy_exceptions.DataError as ex:
+        logging.warning(f'Failed to retrieve restaurant for update for "/restaurants/{rest_id}" endpoint')
+        logging.warning(f'Exception was thrown: {str(ex)}')
+        ret_val = {'success': False,
+                   'message': f'Restaurant with id number {rest_id} could not be retrieved',
+                   }
+        return jsonify(ret_val), 400
+
+    if not rest:
+        ret_val = {'success': False, 'message': f'No restaurant with id {rest_id} found'}
+        return jsonify(ret_val), 404
+
+    json_dict = None
+    try:
+        json_dict = request.json
+    except werkzeug_exceptions.BadRequest as ex:
+        logging.warning('Could not decode the json content')
+        logging.warning(str(ex))
+        ret_val = {'success': False, 'message': str(ex)}
+        return jsonify(ret_val), 400
+
+    rest.name = json_dict.get('name', rest.name)
+    rest.street = json_dict.get('street', rest.street)
+    rest.suite = json_dict.get('suite', rest.suite)
+    rest.city = json_dict.get('city', rest.city)
+    rest.state = json_dict.get('state', rest.state)
+    rest.zip_code = json_dict.get('zip_code', rest.zip_code)
+    rest.phone_num = json_dict.get('phone_num', rest.phone_num)
+    rest.website = json_dict.get('website', rest.website)
+    rest.email = json_dict.get('email', rest.email)
+    rest.date_established = json_dict.get('date_established', rest.date_established)
+
+    ## TODO. Check that restaurant name is not an empty string
+
+    db.session.add(rest)
+    db.session.commit()
+
+    ret_val = {'success': True,
+               'id': rest.id,
+               'message': f"Restaurant updated: {rest.name}"
+               }
+    return jsonify(ret_val), 200
+
 @app.errorhandler(400)
 def bad_request(error):
     logging.warning(error)
