0906b66b39d080d395f70837cac05407e97f2e1a
Earl Fong <earl_fong@operamail.com>
2020-11-29 22:22:18 -0800

    Start implementing 'password' grant type for testing

--- tests/test_restaurants.py
+++ tests/test_restaurants.py
@@ -4,11 +4,17 @@ import re
 from dotenv import load_dotenv, find_dotenv
 
 import unittest
+
 import get_auth0_token
+from get_auth0_token import get_auth0_access_token
+from get_auth0_token import AUTH0_PASSWORD_GRANT
+from get_auth0_token import AUTH0_CLIENT_CRED_GRANT
+from get_auth0_token import AUTH0_CRU_RESTAURANTS_SCOPE
 
 # Get the Auth0 access token just once to avoid repeated hits to the Auth0 API
-access_token = get_auth0_token.get_auth0_access_token()
-auth_header = {'authorization': 'Bearer ' + access_token}
+# "cru" means permissions to create-read-update
+access_token_cru_restaurants = get_auth0_access_token(AUTH0_PASSWORD_GRANT, AUTH0_CRU_RESTAURANTS_SCOPE)
+auth_header_cru_restaurants = {'authorization': 'Bearer ' + access_token_cru_restaurants}
 
 
 def set_environment_vars():
@@ -60,7 +66,7 @@ class RestaurantsTestCases(unittest.TestCase):
 
     def test_get_no_restaurants(self):
         """Test getting list of restaurants when there are none"""
-        resp = self.test_client.get(self.API_V1_BASE, headers=auth_header)
+        resp = self.test_client.get(self.API_V1_BASE, headers=auth_header_cru_restaurants)
         self.assertEqual(resp.status_code, 200)
 
         resp_dict = json.loads(resp.data)
@@ -81,7 +87,7 @@ class RestaurantsTestCases(unittest.TestCase):
         db.session.add(Restaurant(name=name_2))
         db.session.commit()
 
-        resp = self.test_client.get(self.API_V1_BASE, headers=auth_header)
+        resp = self.test_client.get(self.API_V1_BASE, headers=auth_header_cru_restaurants)
         self.assertEqual(resp.status_code, 200)
         resp_dict = json.loads(resp.data)
         self.assertEqual(len(resp_dict['restaurants']), 2)
@@ -98,7 +104,7 @@ class RestaurantsTestCases(unittest.TestCase):
         db.session.commit()
 
         # Since this is a freshly created table, the first id should be 1
-        resp = self.test_client.get(self.API_V1_BASE + '/1', headers=auth_header)
+        resp = self.test_client.get(self.API_V1_BASE + '/1', headers=auth_header_cru_restaurants)
         self.assertEqual(resp.status_code, 200)
         resp_dict = json.loads(resp.data)
         self.assertEqual(resp_dict['restaurant']['name'], name_1)
@@ -114,23 +120,23 @@ class RestaurantsTestCases(unittest.TestCase):
 
         # Since this is a freshly created table, the only id should be 1.
         # id 2 does not exist.
-        resp = self.test_client.get(self.API_V1_BASE + '/2', headers=auth_header)
+        resp = self.test_client.get(self.API_V1_BASE + '/2', headers=auth_header_cru_restaurants)
         self.assertEqual(resp.status_code, 404)
 
     def test_get_restaurant_by_id_not_number(self):
         """Test getting a restaurant by a non-integer id number"""
-        resp = self.test_client.get(self.API_V1_BASE + '/hello', headers=auth_header)
+        resp = self.test_client.get(self.API_V1_BASE + '/hello', headers=auth_header_cru_restaurants)
         self.assertEqual(resp.status_code, 400)
 
     def test_get_restaurant_404_not_found(self):
         """Test getting a restaurant with a non-existent subpath"""
-        headers = {'authorization': 'Bearer ' + access_token}
-        resp = self.test_client.get(self.API_V1_BASE + '/1/hello', headers=auth_header)
+        headers = {'authorization': 'Bearer ' + access_token_cru_restaurants}
+        resp = self.test_client.get(self.API_V1_BASE + '/1/hello', headers=auth_header_cru_restaurants)
         self.assertEqual(resp.status_code, 404)
 
     def test_get_restaurant_405_method_not_allowed(self):
         """Test using POST method that is not allowed"""
-        resp = self.test_client.post(self.API_V1_BASE,headers=auth_header)
+        resp = self.test_client.post(self.API_V1_BASE, headers=auth_header_cru_restaurants)
         self.assertEqual(resp.status_code, 405)
         resp_dict = json.loads(resp.data)
         self.assertEqual(resp_dict['success'], False)
@@ -145,7 +151,7 @@ class RestaurantsTestCases(unittest.TestCase):
     def test_create_restaurant_with_name_only(self):
         """Test creation of a restaurant with only the name field"""
         headers = {'Content-Type': 'application/json'}
-        headers.update(auth_header)
+        headers.update(auth_header_cru_restaurants)
         name = 'Restaurant Chinois'
         info = {'name': name}
         resp = self.test_client.post(self.API_V1_BASE + '/create', headers=headers, data=json.dumps(info))
@@ -157,7 +163,7 @@ class RestaurantsTestCases(unittest.TestCase):
     def test_create_restaurant_with_all_fields(self):
         """Test creation of a restaurant with fields provided"""
         headers = {'Content-Type': 'application/json'}
-        headers.update(auth_header)
+        headers.update(auth_header_cru_restaurants)
         name = 'Restaurant Chinois'
         street = '999 Sutter St'
         suite = '510'
@@ -180,7 +186,7 @@ class RestaurantsTestCases(unittest.TestCase):
         self.assertNotEqual(re.search(name, resp_dict['message']), None)
 
         # Retrieve the restaurant and assert that all fields are as created
-        resp = self.test_client.get(self.API_V1_BASE + '/1', headers=auth_header)
+        resp = self.test_client.get(self.API_V1_BASE + '/1', headers=auth_header_cru_restaurants)
 
         self.assertEqual(resp.status_code, 200)
         resp_dict = json.loads(resp.data)
@@ -199,7 +205,7 @@ class RestaurantsTestCases(unittest.TestCase):
     def test_create_restaurant_no_name(self):
         """Test creation of a restaurant with only the name field"""
         headers = {'Content-Type': 'application/json'}
-        headers.update(auth_header)
+        headers.update(auth_header_cru_restaurants)
         info = {'city': 'Chicago'}
         resp = self.test_client.post(self.API_V1_BASE + '/create', headers=headers, data=json.dumps(info))
         self.assertEqual(resp.status_code, 400)
@@ -222,7 +228,7 @@ class RestaurantsTestCases(unittest.TestCase):
         db.session.commit()
 
         headers = {'Content-Type': 'application/json'}
-        headers.update(auth_header)
+        headers.update(auth_header_cru_restaurants)
         website = 'www.mexicano-nj.com'
         email = 'mexicano-nj@gmail.com'
         info = {'website': website, 'email': email}
@@ -233,7 +239,7 @@ class RestaurantsTestCases(unittest.TestCase):
         self.assertEqual(resp_dict['id'], 1)
         self.assertNotEqual(re.search(name, resp_dict['message']), None)
 
-        resp = self.test_client.get(self.API_V1_BASE + '/1', headers=auth_header)
+        resp = self.test_client.get(self.API_V1_BASE + '/1', headers=auth_header_cru_restaurants)
 
         self.assertEqual(resp.status_code, 200)
         resp_dict = json.loads(resp.data)
@@ -251,7 +257,7 @@ class RestaurantsTestCases(unittest.TestCase):
         db.session.commit()
 
         headers = {'Content-Type': 'application/json'}
-        headers.update(auth_header)
+        headers.update(auth_header_cru_restaurants)
         info = {'name': ''}
         resp = self.test_client.put(self.API_V1_BASE + '/1', headers=headers, data=json.dumps(info))
         self.assertEqual(resp.status_code, 400)
@@ -280,12 +286,12 @@ class RestaurantsTestCases(unittest.TestCase):
         db.session.commit()
 
         # Since this is a freshly created table, the first id should be 1
-        resp = self.test_client.delete(self.API_V1_BASE + '/1', headers=auth_header)
+        resp = self.test_client.delete(self.API_V1_BASE + '/1', headers=auth_header_cru_restaurants)
         self.assertEqual(resp.status_code, 200)
         resp_dict = json.loads(resp.data)
         self.assertEqual(resp_dict['success'], True)
 
-        resp = self.test_client.get(self.API_V1_BASE + '/1', headers=auth_header)
+        resp = self.test_client.get(self.API_V1_BASE + '/1', headers=auth_header_cru_restaurants)
         self.assertEqual(resp.status_code, 404)
         resp_dict = json.loads(resp.data)
         self.assertEqual(resp_dict['success'], False)
@@ -293,7 +299,7 @@ class RestaurantsTestCases(unittest.TestCase):
     def test_delete_restaurant_by_id_none(self):
         """Test deleting a restaurant by a non-existent id number"""
         # Since this is a freshly created table, there are no restaurants
-        resp = self.test_client.delete(self.API_V1_BASE + '/1', headers=auth_header)
+        resp = self.test_client.delete(self.API_V1_BASE + '/1', headers=auth_header_cru_restaurants)
         self.assertEqual(resp.status_code, 404)
 
     def test_delete_restaurant_unauthorized(self):
